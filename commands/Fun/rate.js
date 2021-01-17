3ed98400c68714073893a45fb99cff15c9c4cb6f
Anthony Stainton <anthonystainton@gmail.com>
2021-01-17 14:14:34 -0800

    Update rate.js

--- commands/Fun/rate.js
+++ commands/Fun/rate.js
@@ -3,7 +3,9 @@ const filename = require('path').basename(__filename).split(".")[0]
 exports.run = async (client, message, args, level) => {
     if(!args[0]) return message.channel.send(`Invalid arguments. I can't rate nothing!`)
     const rating = client.randomNumber(0,10)
-    message.channel.send(`I give \`${args.join(" ")}\` a \`${rating}/10\``)
+    let thing = `${args.join(" ")}`
+    if(!thing.includes("<@&")){if(thing.includes("<@")) thing = message.guild.members.cache.get(args.join(" ").replace(/\D/g,'')).user.username}
+    message.channel.send(`I give \`${thing}\` a \`${rating}/10\``)
 }
 
 exports.conf = {
