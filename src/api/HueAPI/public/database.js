5e3f34f92eea3dc96d560606348743e3649bffb6
Anthony Stainton <anthonystainton@gmail.com>
2021-01-24 21:43:50 -0800

    hue api

--- src/api/HueAPI/public/database.js
+++ src/api/HueAPI/public/database.js
@@ -1,14 +1,10 @@
 module.exports = (client, app) => {
-    return
-    app.post('/run', async (req, res) => {
-        console.log('input request');
-        try{
-          const content = req.body
-          const rawEval = eval(content.data);
-          const clean = await client.clean(client, rawEval);
-          res.json({status: "success", result : clean, code: "200"})
-        }catch(error){
-          res.json({status: "error", result : error.name + " : " + error.message, code: "400"})
+    return app.get("/api/user/:discordID", async (req, res) =>{
+        const raw = await client.database.verify.read(req.params.discordID)
+        console.log(raw);
+        if(raw[0] == true){
+            res.json({status: "ok", code: "200", robloxID: raw[1].RobloxID})
         }
+        else res.json({status: "Not Found", code: "404", robloxID: raw[1].RobloxID})
     })
 }
