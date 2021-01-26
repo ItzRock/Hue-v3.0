2d782a33098ed158f8e048f884b243e99f8f9a6e
Anthony Stainton <anthonystainton@gmail.com>
2021-01-25 20:51:09 -0800

    Update database.js

--- src/api/HueAPI/public/database.js
+++ src/api/HueAPI/public/database.js
@@ -1,7 +1,6 @@
 module.exports = (client, app) => {
     return app.get("/api/user/:discordID", async (req, res) =>{
         const raw = await client.database.verify.read(req.params.discordID)
-        console.log(raw);
         if(raw[0] == true){
             res.json({status: "ok", code: "200", robloxID: raw[1].RobloxID})
         }
