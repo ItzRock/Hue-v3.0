2657796de79c6a093e20d142dce64f9995d6225c
Anthony Stainton <anthonystainton@gmail.com>
2021-01-28 21:06:07 -0800

    Update blacklist.js

--- src/database/blacklist.js
+++ src/database/blacklist.js
@@ -2,5 +2,16 @@ module.exports = (client) => {
     const MongoClient = require('mongodb').MongoClient;
     const url = client.config.database[0]
     const dbName = client.config.database[1]
-    const collections = "blacklist"
+    const collections = "global bans discords"
+    client.blacklist = {
+        ban: (userID, reason) => {
+
+        },
+        unban: (userID) => {
+
+        },
+        read: (userID) => {
+
+        },
+    }
 }
