607b4ece75fe9fd3dee6556d00d7da7e41f40199
Anthony Stainton <anthonystainton@gmail.com>
2021-01-30 11:45:04 -0800

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -93,7 +93,7 @@ Made By: ItzRock#0002
             name: "verification",
             catagory: "Verification", 
             value: false, 
-            editable: true
+            editable: false
         },
         "groupID": {
             name: "groupID",
