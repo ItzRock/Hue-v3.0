2877a9662fef0846e64cd3014100b6ed4fff5b51
Anthony Stainton <anthonystainton@gmail.com>
2021-01-30 11:09:26 -0800

    more economy commands

---
+++ commands/Items/inventory.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require('discord.js');
+const filename = require('path').basename(__filename).split(".")[0]
+exports.run = async (client, message, args, level) => {
+    const embed = new MessageEmbed()
+        .setAuthor(client.user.username, client.user.avatarURL())
+        .setFooter(client.user.username, client.user.avatarURL())
+        .setColor(client.embedColour())
+        .setTimestamp()
+        .setThumbnail(client.user.avatarURL())
+        .setTitle(`Your Hue Inventory`)
+    const data = await client.Inventory.fetch(message.author.id)
+    if(data.length == 0){
+        embed.addFields(
+            { name: `**Looks like you have none**`, value: `Go find some items to buy with ${message.settings.prefix.value}buy`, inline: true },
+        )
+        return message.channel.send(embed)
+    }
+    const items = await client.items.readAll()
+    data.forEach(rawitem => {
+        const item = items.find(i => i.Name == rawitem.ItemName)
+        embed.addFields(
+            { name: `**${item.Name}**`, value: `${item.Description}`, inline: true },
+        )
+    });
+    return message.channel.send(embed)
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ["inv", "items"],
+    permLevel: "User",
+    disablable: true,
+    premium: false
+};
+exports.help = {
+    name: filename,
+    category: __dirname.split("\\")[__dirname.split("\\").length - 1],
+    description: "View your purchased items through the store command",
+    usage: `${filename}`
+};
---
+++ commands/Items/use.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require('discord.js');
+const filename = require('path').basename(__filename).split(".")[0]
+exports.run = async (client, message, args, level) => {
+    if(!args[0]) return message.channel.send("Specify the item you want to use.");
+    const use = args.join(" ").toLowerCase();
+    const itemObjects = await client.items.readAll();
+    const items = []
+    itemObjects.forEach(item =>{
+        items.push(item.Name)
+    })
+    if(items.includes(use.toLowerCase()) == false) return message.channel.send("That item doesn't exist!")
+    const inventory = await client.Inventory.fetch(message.author.id)
+    let itemAmount = 0
+    inventory.forEach(item => {
+        if(item.ItemName == use) itemAmount++;
+    })
+    if(itemAmount == 0) return message.channel.send("You don't own that item!")
+    const item = itemObjects.find(item => item.Name == use)
+    if(item.DeleteOnUse == true){client.Inventory.remove(use, message.author.id);}
+    if(item.onUseFunction == undefined) return message.channel.send(`You tried using ${item.Name} but vanished as it had no use.`)
+    eval(item.onUseFunction);
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "User",
+    disablable: true,
+    premium: false
+};
+exports.help = {
+    name: filename,
+    category: __dirname.split("\\")[__dirname.split("\\").length - 1],
+    description: "use an item from your inventory",
+    usage: `${filename} <item>`
+};
