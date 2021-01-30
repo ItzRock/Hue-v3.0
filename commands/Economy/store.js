0099e14e5bb6c6e1a3471ba166da87ab11eda375
Anthony Stainton <anthonystainton@gmail.com>
2021-01-29 19:07:35 -0800

    store

---
+++ commands/Economy/store.js
@@ -0,0 +1,44 @@
+const { MessageEmbed } = require('discord.js');
+const { titleCase } = require("title-case");
+const filename = require('path').basename(__filename).split(".")[0]
+exports.run = async (client, message, args, level) => {
+    const items = await client.items.readAll()
+    const lines = ["Some people call this junk. Me? I call it treasure.", "Take a look.", "Remember… I’ll give you the best deals or die trying", "Here's what I can spare.", "See for yourself", "Everything I've got on display, really.", "Just what you see here.", "Trinkets, odds and ends, that sort of thing.", "Oh, a bit of this and a bit of that."]
+    const line = lines[Math.round(client.randomNumber(0, lines.length -1))]
+    const embed = new MessageEmbed()
+        .setAuthor(client.user.username, client.user.avatarURL())
+        .setFooter(client.user.username, client.user.avatarURL())
+        .setColor(client.embedColour("safe"))
+        .setTimestamp()
+        .setThumbnail(client.user.avatarURL())
+        .setTitle(`${client.user.username} Store`)
+        .setDescription(line)
+    items.forEach(item =>{
+        if(item.Available == false) return
+        const newname = titleCase(item.Name)
+        embed.addFields(
+            { name: `**${newname}**`, value: `Description:\n\`${item.Description}\`\nPrice:\`$${item.Price}\``, inline: true },
+        )
+    });
+    if(!args[0]){
+    return message.channel.send(embed)
+    }else if(args[0] == "add"){
+        if(level !== 5) return message.channel.send("Missing Permissions.");
+
+    }
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ["shop"],
+    permLevel: "User",
+    disablable: true,
+    premium: false
+};
+exports.help = {
+    name: filename,
+    category: __dirname.split("\\")[__dirname.split("\\").length - 1],
+    description: "good ol' hue store",
+    usage: `${filename}`
+};
---
+++ src/database/items.js
@@ -0,0 +1,95 @@
+module.exports = (client) => {
+    const MongoClient = require('mongodb').MongoClient;
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'Hue';
+    client.items = {}
+
+    client.items.readAll = function(){
+        let promise = new Promise((resolve, reject) => {
+            MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
+                const db = client.db(dbName);
+                db.collection("items").find().toArray(function(err, result) {
+                    if (err) throw err;
+                    resolve(result)
+                });
+            });
+        });
+        return promise.then((value) => {
+            return value
+        });  
+    }
+
+    client.items.remove = function(discordID){
+        let promise = new Promise((resolve, reject) => {
+            MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
+                const db = client.db(dbName);
+                let query = { DiscordID: discordID.toString() };
+                db.collection("items").deleteOne(query, function(err, obj) {
+                    if (err) throw err;
+                    resolve('Successfully Removed')
+                });
+            });
+        });
+        return promise.then((value) => {
+            return value
+        });
+    }
+    /*
+        User Inventory
+    */
+    client.Inventory = {}
+
+    // Add an item
+
+    client.Inventory.add = async function(item, discordID){
+        const rawItems = await client.items.readAll()
+        const items = []
+        rawItems.forEach(i => {
+            items.push(i.Name)
+        });
+        if(items.includes(item) == false) return false;
+        let promise = new Promise((resolve, reject) => {
+            MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
+                const db = client.db(dbName);
+                let items = { DiscordID: discordID.toString(), ItemName: item.toString()};
+                db.collection("inventories").insertOne(items, function(err, res) {
+                    if (err) console.log(err);;
+                    resolve(true)
+                });
+            });
+        });
+        return promise.then((value) => {
+            return value
+        }); 
+    } 
+    client.Inventory.fetch = async function(discordID){
+        let promise = new Promise((resolve, reject) => {
+            MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
+                const db = client.db(dbName);
+                let query = { DiscordID: discordID.toString() };
+                db.collection("inventories").find(query).toArray(function(err, result) {
+                    if (err) throw err;
+                    resolve(result)
+                });
+            });
+        });
+        return promise.then((value) => {
+            return value
+        });  
+    }
+    client.Inventory.remove = async function(item, discordID){
+        let promise = new Promise((resolve, reject) => {
+            MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
+                const db = client.db(dbName);
+                let query = { DiscordID: discordID.toString(), ItemName: item.toString() };
+                db.collection("inventories").findOneAndDelete(query, function(err, obj) {
+                    if (err) resolve(err);
+                    resolve(true)
+                });
+            });
+        });
+        return promise.then((value) => {
+            return value
+        });  
+    }
+}
