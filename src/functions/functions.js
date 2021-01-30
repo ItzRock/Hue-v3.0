99be6e82532a9f551b068529e77cf3f8480f0ca8
Anthony Stainton <anthonystainton@gmail.com>
2021-01-29 19:10:43 -0800

    Update functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -291,5 +291,7 @@ module.exports = (client) => {
     if(err.msg == "Missing Permissions") return // ksfhg SHUT UP ABOUT MISSING PERMISSIONS OR I WILL DROP KICK YOU IN THE BALLS
     const clean = await client.clean(client, err);
     client.channels.cache.get(client.config.errorChannel).send(`\`\`\`js\n${clean.substring(0, 1500)}\n\`\`\``)
+    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    client.logger.error(`Unhandled Rejection: ${errorMsg}`);
   });
 };
