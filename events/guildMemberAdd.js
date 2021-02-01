af486597cbff092b5b407bd04cff8eace0aeb6ad
Anthony Stainton <anthonystainton@gmail.com>
2021-01-31 21:46:44 -0800

    static embed option for welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,11 +1,37 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = async (client, member) => {
     const settings = client.getSettings(member.guild);
-    console.log(settings.welcoming);
     if(settings.welcoming.value == true){
         const welcomingChannel = client.getChannel(member.guild, settings["welcoming-channel"].value);
         if(welcomingChannel !== undefined){
             const welcomeMessage = customText(settings["welcoming-text"].value)
-            welcomingChannel.send(welcomeMessage)
+            if(welcomeMessage == "--embed"){
+                let desciption
+                if(settings.GroupJoinRequired.value == true) desciption = `
+                In order to gain access to the rest of the channels please follow these steps.\n
+                **Step 1:** Read the rules,
+                **Step 2:** Join our group linked [here,](https://www.roblox.com/groups/${settings.groupID.value})
+                **Step 3:** Run \`${settings.prefix.value}verify\`.
+                **Step 4:** boom your in
+                `
+                else desciption = `
+                In order to gain access to the rest of the channels please follow these steps.\n
+                **Step 1:** Read the rules,
+                **Step 2:** \`Run ${settings.prefix.value}verify\`.
+                **Step 3:** boom your in
+                `
+                const embed = new MessageEmbed()
+                    .setAuthor(client.user.username, client.user.avatarURL())
+                    .setFooter(client.user.username, client.user.avatarURL())
+                    .setTimestamp()
+                    .setColor(client.embedColour())
+                    .setThumbnail(member.guild.iconURL())
+                    .setTitle(`Welcome \`${member.user.username}\` to **${member.guild.name}!**`)
+                    .setDescription(desciption)
+                setTimeout(function(){welcomingChannel.send(embed)}, 1000)
+            }
+            else setTimeout(function(){welcomingChannel.send(welcomeMessage)}, 1000)
         }
     }
     if(settings.unverifiedRole.value !== undefined || settings.unverifiedRole.value !== false){
