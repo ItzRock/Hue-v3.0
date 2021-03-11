52873e087ba8a43339127848df440d430c4de098
Anthony Stainton <anthonystainton@gmail.com>
2021-03-11 13:03:47 -0800

    Update reboot.js

--- commands/Hue Admins/reboot.js
+++ commands/Hue Admins/reboot.js
@@ -1,5 +1,7 @@
 exports.run = async (client, message, args, level) => { 
     await message.reply("Bot is shutting down.");
+    client.activeStatus =`rebooting. wait for status change before using commands`
+    await client.user.setActivity(client.activeStatus, {type: "PLAYING"});
     process.exit(0);
   };
   
