632928083fdd4806318265935f21364eac2d556c
ItzRock <anthonystainton@gmail.com>
2021-03-14 03:14:53 +0000

    changed memory usage for stats

--- commands/Hue Admins/stats.js
+++ commands/Hue Admins/stats.js
@@ -1,24 +1,28 @@
 const { version } = require("discord.js");
 const moment = require("moment");
 const MessageEmbed = require("discord.js").MessageEmbed
+const os = require("os")
 require("moment-duration-format");
 
 exports.run = async (client, message, args, level) => {
+  function formatBytes(a,b=2){if(0===a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return parseFloat((a/Math.pow(1024,d)).toFixed(c))+""+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}
+
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  const usage = Math.round(((require('os').totalmem() - require('os').freemem()) / 1024 / 1024).toFixed(2))
-  const total = Math.floor((require("os").totalmem() / 1024 / 1024).toFixed(2));
+  const hueUsed = formatBytes(process.memoryUsage().heapUsed)
+  const total = formatBytes(os.totalmem())
+  const usage = formatBytes(os.totalmem() - os.freemem())
+  const percentage = Math.round(((os.totalmem() - os.freemem()) / os.totalmem()) * 100)
   let statisticsEmbed = new MessageEmbed()
     .setColor(client.embedColour("safe"))
     .setTitle(`${client.user.username} Statistics`)
     .addFields(
       { name: `Users Cached`, value: `${client.users.cache.size.toLocaleString()}`, inline: true },
       { name: `Users Verified`, value: `${await client.database.verify.countALL()}`, inline: true },
-      { name: `System Memory Usage`, value: `${usage}/${total} MB (${Math.floor((usage / total)*100)}%)\n${client.user.username} Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, inline: true },
       { name: `${client.user.username} Uptime`, value: `${duration}`, inline: true },    
       { name: `Servers Cached`, value: `${client.guilds.cache.size.toLocaleString()}`, inline: true },
-      { name: `Channels Cached`, value: `${client.channels.cache.size.toLocaleString()}`, inline: true },
       { name: `Discord.js Version`, value: `v${version}`, inline: true },
       { name: `Node Version`, value: `${process.version}`, inline: true },
+      { name: `System Memory Usage`, value: `${usage}/${total} (${percentage}%)\n${client.user.username} Usage: ${hueUsed}`, inline: true },
       { name: `System OS`, value: `${process.platform}`, inline: true }
     )
     .setTimestamp()
