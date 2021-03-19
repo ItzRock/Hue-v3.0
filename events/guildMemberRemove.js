948579ba5b2a5acba73368dcb17684a37b8de163
ItzRock <anthonystainton@gmail.com>
2021-03-19 04:51:46 +0000

    guild member leaving is now appear in guild logs

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,21 @@
+module.exports = async (client, member) => {
+    const settings = await client.getSettings(member.guild);
+    if(settings.logs.value === undefined) return// no logs
+    const logs = client.getChannel(member.guild, settings.logs.value);
+    if(logs === undefined) return // Logs is not set up correctly
+
+    const embed = client.defaultEmbed()
+        .setTitle(`Member Leave`)
+        .setColor("RED")
+        .setThumbnail(member.user.avatarURL())
+        .addFields(
+            {name: "User:", value: `\`${member.user.tag}\``, inline: true},
+        )
+    const dbInfo = await client.database.verify.read(member.user.id)
+    if(dbInfo[0] == false){
+        embed.addField(`Database Information`, "No information was found", true)
+    } else {
+        embed.addField(`Database Information`, `**Roblox Username:** \`${dbInfo[1].RobloxUsername}\``, true)
+    }
+    logs.send(embed)
+}
--- events/message.js
+++ events/message.js
@@ -8,7 +8,7 @@ module.exports = async (client, message) => {
   
     const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
     if (message.content.match(prefixMention)) {
-      return message.reply(`:wave: my prefix is: \`${settings.prefix.value}\``);
+      return message.reply(`:wave: hiii!! My prefix is: \`${settings.prefix.value}\``);
     }
     // not for us
     if (message.content.indexOf(settings.prefix.value) !== 0) return;
@@ -31,17 +31,20 @@ module.exports = async (client, message) => {
 
     if(cmd.conf.enabled == false) return;
     if (cmd && !message.guild && cmd.conf.guildOnly)
-      return message.channel.send("This command is unavailable in DMs. Please run in a Server.");
+      return message.channel.send(`${client.config.emojis.x} This command is unavailable in DMs. Please run in a Server.`);
     const disabledCommands = message.settings["disabled-commands"]
     if(disabledCommands.value.includes(cmd.help.name)){
-      return message.channel.send(`This command has been disabled in this guild! please try another guild or in DMs.`)
+      if(client.config.AuthorizedUsers.includes(message.author.id)){
+        message.channel.send(`${client.config.emojis.check} This command is disabled but \`${message.author.tag}\` is allowed. (Case: ${client.user.username} Administrator)`)
+      }
+      else return message.channel.send(`${client.config.emojis.x} This command has been disabled in this guild! please try another guild or in DMs.`)
     } 
     const premiumServer = message.settings["premium"]
     if(cmd.conf.premium == true && premiumServer.value == false && !client.config.AuthorizedUsers.includes(message.author.id)) 
-      return message.channel.send(`This command can only be used in premium servers.`)
+      return message.channel.send(`${client.config.emojis.x} This command can only be used in premium servers.`)
 
     if (level < client.levelCache[cmd.conf.permLevel]) {
-        return message.channel.send(`Invalid Permission Level.
+        return message.channel.send(`${client.config.emojis.x} Invalid Permission Level.
     Your permission level is ${level} (${client.config.permissionLevels.find(l => l.level === level).name})
     This command requires level ${client.levelCache[cmd.conf.permLevel]} (${cmd.conf.permLevel})`);
       
@@ -53,13 +56,12 @@ module.exports = async (client, message) => {
       message.flags.push(args.shift().slice(1));
     }
     if(message.channel.type == "dm"){
-      client.logger.cmd(`GUILD:${message.channel.type} | L${level} ${message.author.username} ran ${cmd.help.name} ${args.join(" ")}`);
+      client.logger.cmd(`GUILD: ${message.channel.type} | L${level} ${message.author.username} ran ${cmd.help.name} ${args.join(" ")}`);
     }
     else client.logger.cmd(`GUILD: ${message.guild.name} | L${level} ${message.author.username} ran ${cmd.help.name} ${args.join(" ")}`);
     try {
       cmd.run(client, message, args, level); 
     } catch (error) {
-      
       message.channel.send(`An error has occured in the command! ${error.name}: ${error.message}. PLEASE REPORT THIS!`)
       client.logger.log(`COMMAND ERROR => ${error.name}: ${error.message}`)
     }
