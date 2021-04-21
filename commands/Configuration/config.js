8b3f837c617dbecf204926a2ecfe0aebd43ebd3c
HarryXChen <51322624+HarryXChen3@users.noreply.github.com>
2021-04-20 20:43:46 -0400

    Update config.js

--- commands/Configuration/config.js
+++ commands/Configuration/config.js
@@ -1,79 +1,78 @@
-const { MessageEmbed } = require('discord.js');
-const { type } = require('os');
 const filename = require('path').basename(__filename).split(".")[0]
-exports.run = async (client, message, args, level) => {
-    const settings = Object.values(message.settings) // Grabs current server settings and puts it into an array
-    const defaults = client.config.defaultSettings;
-    if(!args[0]){ // Show All Settings.
-        const embed = new MessageEmbed()
+exports.run = (client, message, args, level) => {
+    // If no specific command is called, show all filtered commands.
+    if (!args[0] || args[0] == "expanded" || args[0] == "all") {
+        const avaliableCmds = [];
+        const mappedCommands = [];
+        const disabledCmds = message.settings["disabled-commands"].value;
+        client.commands.forEach(cmd => {
+            if (client.levelCache[cmd.conf.permLevel] > level) return;
+            if (message.channel.type == "dm" && cmd.conf.guildOnly !== true) return;
+
+            if (disabledCmds.includes(cmd.help.name)) return; // Disabled in guild
+            if (cmd.conf.enabled == false) return; // Disabled command
+            if (cmd.conf.premium === true && message.settings.premium.value !== true) return; // Not premium server
+            
+            avaliableCmds.push(cmd)
+        });
+
+        let output = client.defaultEmbed()
             .setColor(client.embedColour())
-            .setTitle(`Current settings for: ${message.guild.name}`)
-            .setThumbnail(message.guild.iconURL())
-            .setDescription(`Capitalization is important as these values are cap sensitive.`)
-            .setFooter('Statistics updated at', client.user.avatarURL())
-            .setAuthor(client.user.username, client.user.avatarURL())
+            .setTitle(`Commands List - ${client.user.username}`)
+            .setDescription((disabledCmds.length === 0) ? `\`[Use ${message.settings.prefix.value}help [commandname] for details]\`` : `\`[Use ${message.settings.prefix.value}help [commandname] for details]\`\n\`Note: ${disabledCommands.length} command(s) are disabled in this guild.\``)
+            .setThumbnail(client.user.avatarURL())
+            .setTimestamp();
+            
+        const iteratedCategories = [];
+        avaliableCmds.forEach(cmd => {
+            const cmdCategory = cmd.help.category
+            if (!iteratedCategories.includes(cmdCategory)) {
+                iteratedCategories.push(cmdCategory); mappedCommands[cmdCategory] = {name: cmdCategory, fields: []}
+            }; mappedCommands[cmdCategory].fields.push(`\`${cmd.help.name}\``)
+        })
+
+        for (let cmdCategory in mappedCommands) {
+            let mappedArray = mappedCommands[cmdCategory]
+            if (mappedArray.fields.length == 0) return;
+            if (mappedArray.fields[mappedArray.fields.length]) mappedArray.fields[mappedArray.fields.length].replace(",", "");
+            output.addField(`${mappedArray.name}`, mappedArray.fields.join(", "), true)
+        }; message.channel.send(output);
+    } else {
+      // Show individual command's help.
+      let command = args[0];
+      if (client.commands.has(command) || client.aliases.has(command)) {
+        command = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+        let output = client.defaultEmbed()
+            .setColor(client.embedColour())
+            .setTitle(command.help.name)
+            .addFields(
+                { name: 'Command name', value: `\`${command.help.name}\u200b\``, inline:true },
+                { name: 'Description', value: `\`${command.help.description}\u200b\``, inline:true },
+                { name: 'Permission Level', value: `\`${command.conf.permLevel}\u200b\``, inline:true },
+                { name: 'Usage', value: `\`${command.help.usage}\u200b\``, inline:true},
+                { name: 'Aliases', value: `\`${command.conf.aliases.join("\`, \`")}\u200b\``, inline:true},
+                { name: 'Type', value: `\`${command.help.category}\u200b\``, inline:true},
+                { name: 'Premium Only Command?', value: `\`${command.conf.premium}\u200b\``, inline:true},
+                { name: 'Can Be Disabled?', value: `\`${command.conf.disablable}\u200b\``, inline:true},
+            )
             .setTimestamp()
-        // Get the catagories
-        const catagories = []
-        const catagoryNames = []
-        settings.forEach(setting => {
-            const catagory = setting.catagory 
-            if(catagory == "" || catagory == undefined) return;
-            if(setting.editable === false) return "Not editable."
-            if(catagoryNames.includes(catagory)) return "Already have that catagory."
-            catagories.push({name: catagory, fields: []})
-            catagoryNames.push(catagory)
-        })
-        // Add the keys to the catagories
-        catagories.forEach(catagory =>{
-            settings.forEach(key =>{
-                if(key.disablable == false) return
-                if(key.catagory != catagory.name) return
-                if(key.value == undefined) key.value = `Value not set.`
-                if(key.value == true) key.value = `Enabled.`
-                if(key.value == false) key.value = `Disabled.`
-                catagory.fields.push(`**${key.name}:**\n\`${key.value}\`\n`)
-            })
-        })
-
-        // Add the fields
-        catagories.forEach(catagory => {
-            embed.addField(`${catagory.name}`, catagory.fields.join("\n"), true)
-        })
-
-        message.channel.send(embed)
-    } else{ // Setting a key
-        if (!defaults[args[0]]) return message.reply(client.config.emojis.x+" This key does not exist in the settings");
-        
-        // find key to see if editable.
-        keys = []
-        settings.forEach(key =>{
-            if(key.name == args[0]) return keys.push(key);
-        })
-        if(keys[0].editable == false) return message.reply(`${client.config.emojis.x} This key cannot be edited`)
-        var joinedValue = args.slice(1).join(" ");
-        if(joinedValue == "" || joinedValue == undefined) return message.reply(client.config.emojis.exclamation + "Please provide a value.");
-
-        const objects = [true, false, true, false , true, false]
-        const strings = ["true", "false", "enable", "disable" , "enabled", "disabled"]
-
-        if(strings.includes(joinedValue)) joinedValue = objects[strings.indexOf(joinedValue)]
-        client.enmap.edit(message, joinedValue, args[0])
-        message.reply(`${client.config.emojis.check} ${args[0]} successfully edited to ${joinedValue}`);
+            .setFooter(`[optional] <required>\nCommand info for: "${command.help.name}" updated at`, client.user.avatarURL());
+        message.channel.send(output);
+      }
     }
-}
+  };
 
 exports.conf = {
     enabled: true,
-    guildOnly: true,
-    aliases: [],
-    permLevel: "Administrator",
-    disablable: true,
+    guildOnly: false,
+    aliases: ["cmds", "h", "help"],
+    permLevel: "User",
+    disablable: false,
     premium: false
 };
 exports.help = {
     name: filename,
     category: __dirname.split("\\")[__dirname.split("\\").length - 1].split("/")[__dirname.split("/").length - 1],
-    description: "Modify a guild's current settings.",
-    usage: `${filename} [key] [value]`
+    description: "helps you with getting help",
+    usage: `${filename} [command]`
 };
